ebf073039555c1cbf5acb81d81c61b9c02990047
Anniina-55 <anniina.leikas55@gmail.com>
2026-02-17 17:42:56 +0200

    Project setup

---
+++ app/src/main/java/com/example/scaffold_and_navigation/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.scaffold_and_navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.scaffold_and_navigation.ui.theme.ScaffoldandnavigationTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScaffoldandnavigationTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("My app")
+                            }
+                        )
+                    },
+                ) { innerPadding ->
+                    Greeting("Hello", modifier = Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ScaffoldandnavigationTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/scaffold_and_navigation/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.scaffold_and_navigation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Beetroot = Color(0xFF640025)
+val BeetrootLight = Color(0xFF8A004C)
+val LightPink = Color(0xFF982B6F)
+val Light = Color(0xFFFFF8F8)
